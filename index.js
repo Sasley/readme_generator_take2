2a0661e813a8220d1de5530748ad94ad5eaf7b67
Shirley <90633395+Sasley@users.noreply.github.com>
2023-08-12 22:14:41 -0400

    write function

--- index.js
+++ index.js
@@ -53,9 +53,10 @@ const questions = [
           }   
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 // TODO: Create a function to initialize app
 function init() {}
 
