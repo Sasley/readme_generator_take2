ed348dc1f79f7721bb7d0b2fdc088028d69e3248
Shirley <90633395+Sasley@users.noreply.github.com>
2023-08-12 23:07:16 -0400

    init function

--- index.js
+++ index.js
@@ -7,58 +7,66 @@ const generateMarkdown = require("./utils/README.md");
 // prompt questions
 const questions = [
     {
-        type: 'input', 
-        name: 'title',
-        message: 'Project title?',  
-       },
-       {
         type: 'input',
-        name: 'desciption',
-        message: 'Type detail desciption of your project answering the what, why, and how of your project.',  
-       },
-       {
-           type: 'input',
-           name: 'installation',
-           message: 'What tools are required to install your project? ',  
-       },
-       {
-           type: 'input',
-           name: 'usage',
-           message: 'Provide instructions on how to use your project.',  
-       },
-       {
-           type: 'list',
-           name: 'license',
-           message: 'License?',
-           choices: ['MIT', 'ISC', 'GNUPLv3'],
-           filter(val){
-               return val.toLowerCase();
-           } 
-           {
-            type: 'input',
-            name: 'Contribution',
-            message: 'Name of Contributors and or resources used. ',  
-           },
-           {
-            type: 'input',
-            name: 'Test',
-            message: ' Resource used to test project? ', 
-            {
-                type: 'input',
-                name: 'Questions',
-                message: 'Please type your Github account name. ',  
-            },
-        },
-           
-          }   
+        name: 'title',
+        message: 'Project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Type detail desciption of your project answering the what, why, and how of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What tools are required to install your project? ',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions on how to use your project.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'List of contributors and resources used.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What program did you use to test your project?',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Github account which you can be contacted.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose license?',
+        choices: ['MIT','GPL','Apache'],
+        filter(val){
+        return val.toLowerCase();
+    },
+},
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize app
+// function init() {
+//     inquirer.prompt(questions).then((response) => {
+//         console.log("Creating Professional README.md File...");
+        function init() {
+            inquirer.prompt(questions)
+            .then(function (userInput) {
+                // console.log(userInput)
+                writeToFile("utils/README.md", generateMarkdown(userInput));
+            });
+        }
 
-// Function call to initialize app
-init();
+        // Function call to initialize app
+        init();
